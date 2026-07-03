ffbc5d16939c065ff126363b213a4b106b1902c5
Zhuanghua Shi <shizhuanghua@gmail.com>
2026-07-03 06:53:39 +0000

    Session 2 slides: add GLM t-test recall (interactive) + AI-as-learning segment

---
+++ quartz/static/slides/session-2/assets/swimlanesDiagram-7S2UDSDB-E20N8cQV.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-QQNUYIFB-Ba1DVapl.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-CAXAfHMO.js";import"./chunk-QVUG6NDL-B0omcSNk.js";import"./chunk-RTBOCTTP-Cbmt_yp9.js";import"./chunk-HPLX5OYV-Cd3j3SfA.js";import"./chunk-DRARJEGU-Dtwpp8BJ.js";import"./modules/vue-C8RVo8tB.js";import"./index-CuBN4Qn9.js";import"./modules/shiki-D1SiokL_.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
