120d2b8fb2f30cea03be5f0ab657f423c88af923
Zhuanghua Shi <shizhuanghua@gmail.com>
2026-07-03 06:28:46 +0000

    Hide Slidev nav-controls overlay in embedded tutorial decks

---
+++ quartz/static/slides/session-2/assets/swimlanesDiagram-7S2UDSDB-CQzyc3Kn.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-QQNUYIFB-CEGQj9oi.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-Cy0baWdU.js";import"./chunk-QVUG6NDL-DLFqYiwA.js";import"./chunk-RTBOCTTP-cxhh5AkV.js";import"./chunk-HPLX5OYV-BcF1Fm_I.js";import"./chunk-DRARJEGU-DUrDegfU.js";import"./modules/vue-Ce3nrIif.js";import"./index-D_q3CyX4.js";import"./modules/shiki-DKTbj92o.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
