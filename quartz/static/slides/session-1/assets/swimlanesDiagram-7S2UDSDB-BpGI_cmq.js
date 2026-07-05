6c4e2b870e3847e35193443b3d24b1ddf816cf37
Zhuanghua Shi <shizhuanghua@gmail.com>
2026-07-05 12:11:45 +0000

    Regenerate Session 1 anatomical figures with defaced (brain-only) images

--- quartz/static/slides/session-1/assets/swimlanesDiagram-7S2UDSDB-BpGI_cmq.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-QQNUYIFB-xcTNCVI6.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-D7dHShWY.js";import"./chunk-QVUG6NDL-BLL4cI4q.js";import"./chunk-RTBOCTTP-D6NmunrV.js";import"./chunk-HPLX5OYV-CqXicDjV.js";import"./chunk-DRARJEGU-Bs4ZkKiu.js";import"./modules/vue-CUqn-oEj.js";import"./index-zQbTeUPR.js";import"./modules/shiki-ixno5-dk.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
---
+++ quartz/static/slides/session-1/assets/swimlanesDiagram-7S2UDSDB-DQqnHq_x.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-QQNUYIFB-BYG1WWvy.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-DlDe3TSp.js";import"./chunk-QVUG6NDL-DFa_GT1D.js";import"./chunk-RTBOCTTP-D5EOQTXs.js";import"./chunk-HPLX5OYV-Dmq6Ym1X.js";import"./chunk-DRARJEGU-CiGEIfiM.js";import"./modules/vue-CUqn-oEj.js";import"./index-B9XNECim.js";import"./modules/shiki-ixno5-dk.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
