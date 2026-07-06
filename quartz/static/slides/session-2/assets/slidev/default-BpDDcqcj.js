78d2ef3c482ca49a6fda42319e93b873f2d48693
Zhuanghua Shi <shizhuanghua@gmail.com>
2026-07-06 06:59:14 +0000

    Hide Slidev bottom toolbar (icons render invisible in prod builds, issue #2300) + keep goto-dialog hidden

---
+++ quartz/static/slides/session-2/assets/slidev/default-BpDDcqcj.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-C8RVo8tB.js";import{_ as r}from"../index-CNZNGjL8.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
---
+++ quartz/static/slides/session-2/assets/swimlanesDiagram-7S2UDSDB-CySnnYdh.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-QQNUYIFB-Cfk0pjys.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-ZFA45w39.js";import"./chunk-QVUG6NDL-BsBuHTO4.js";import"./chunk-RTBOCTTP-DGFmf-IL.js";import"./chunk-HPLX5OYV-C99JRdc_.js";import"./chunk-DRARJEGU-BXv7zTyK.js";import"./modules/vue-C8RVo8tB.js";import"./index-CNZNGjL8.js";import"./modules/shiki-D1SiokL_.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
